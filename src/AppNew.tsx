198de64c190f565097dfab47c846cb03bb76fcb6
lastchaptermedia2016 <lastchaptermedia2016@gmail.com>
2026-03-20 19:54:54 +0200

    ADD:AdminDash_imp

--- src/AppNew.tsx
+++ src/AppNew.tsx
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ChatWidget from "./components/widget/ChatWidget";  // ← uncommented
+import AdminDashboard from "./pages/AdminDashboard";
+
 
 const queryClient = new QueryClient();
 

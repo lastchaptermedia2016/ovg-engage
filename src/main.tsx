ef9d2bf8a150de4876aecc73ba3121a0acf86232
lastchaptermedia2016 <lastchaptermedia2016@gmail.com>
2026-03-20 21:15:01 +0200

    ERROR:BLK_Scrn_fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppNew from './AppNew.tsx'
+import AppNew from './AppNew' // ← Haal die .tsx uit
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<AppNew />)
